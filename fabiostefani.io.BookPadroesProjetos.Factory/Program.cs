9d03cf9e54a1bf7900d42ecd3d78fa8786abff7a
fabiostefani@gmail.com <fabiostefani@gmail.com>
2018-05-22 01:03:20 -0300

    Inicio estudos DP Factory

---
+++ fabiostefani.io.BookPadroesProjetos.Factory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace fabiostefani.io.BookPadroesProjetos.Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
