969cfb0919fd5aa4dcdbdc45a43946898f03e543
fabiostefani@gmail.com <fabiostefani@gmail.com>
2018-06-11 22:28:50 -0300

    Padrão Singleton

---
+++ fabiostefani.io.BookPadroesProjetos.Singleton/ChocolateBoiler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabiostefani.io.BookPadroesProjetos.Singleton
+{
+    public class ChocolateBoiler
+    {
+        private static ChocolateBoiler _chocolateBoiler;
+
+        private bool Empty;
+        private bool Boiled;
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            if (_chocolateBoiler == null)
+            {
+                Console.WriteLine("Creating ChocolateBoiler");
+                _chocolateBoiler = new ChocolateBoiler();
+            }
+            Console.WriteLine("Returning ChocolateBoiler");
+            return _chocolateBoiler;
+        }
+
+        private bool IsEmpty()
+        {
+            return Empty;
+        }
+
+        private bool IsBoiled()
+        {
+            return Boiled;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                Boiled = true;
+            }
+        }
+    }
+}
---
+++ fabiostefani.io.BookPadroesProjetos.Singleton/SingletonExampleChargerFast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabiostefani.io.BookPadroesProjetos.Singleton
+{
+    public class SingletonExampleChargerFast
+    {
+        private static SingletonExampleChargerFast _uniqueInstance = new SingletonExampleChargerFast();
+        private SingletonExampleChargerFast()
+        {
+
+        }
+
+        public static SingletonExampleChargerFast GetInstance()
+        {
+            Console.WriteLine("Returning instance SingletonExampleChargerFast");
+            return _uniqueInstance;
+        }
+    }
+}
